5a5f037505e99bb9fd630556325febddc751f758
GSNasciment0 <glayvidsantos@gmail.com>
2024-01-25 17:58:50 -0300

    Coloquei o .toFixed(3) para limitar os caracteres do resultado das somas, mas não está retornando corretamente, ainda não descobri o motivo.

--- olamundo.js
+++ olamundo.js
@@ -22,6 +22,6 @@ let drumis = 4.8 //valor da grama
 console.log("");
 console.log(`Daniel, amigo do Wilden e do Glayvid tem R$ ${daniel} na carteira, já o Wilden tem somente R$ ${wilden} e Glayvid conta com ${glayvid}. Ambos querem tirar as cara e comprar drumis na grama, o ultimo valor cotado está de R$ 4,80 para 1 grama. Quanto de Drumis cada um consegue comprar e quanto seria o total caso eles comprassem o drumis juntos ? `);
 console.log("");
-console.log(`Glayvid com ${glayvid} pode comprar ${glayvid / drumis} gramas.
-Já Daniel com ${daniel} consegue comprar ${daniel / drumis} gramas, e o Wilden ${wilden / drumis} gramas com os R$ ${wilden} que ele tem.
-Se todos unirem as forças conseguem-se comprar o total de ${(glayvid + wilden + daniel) / drumis} gramas totais de Drumis.`);
+console.log(`Glayvid com ${glayvid} pode comprar ${glayvid / drumis.toFixed(3)} gramas.
+Já Daniel com ${daniel} consegue comprar ${daniel / drumis.toFixed(3)} gramas, e o Wilden ${wilden / drumis.toFixed(3)} gramas com os R$ ${wilden} que ele tem.
+Se todos unirem as forças conseguem-se comprar o total de ${(glayvid + wilden + daniel) / drumis.toFixed(3)} gramas totais de Drumis.`);
